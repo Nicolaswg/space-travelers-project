27813faff29396f236524f8ad6e2f89a94ba7358
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-03 02:57:42 +0500

    Add styles to Mission buttons

--- src/components/SingleMission.js
+++ src/components/SingleMission.js
@@ -6,12 +6,15 @@ const MissionTable = (props) => {
     id,
     name,
     description,
+    reserved,
   } = props;
 
   return (
-    <tr id={id}>
-      <td>{name}</td>
+    <tr className={reserved ? 'background-gray' : ''} id={id}>
+      <td className="mission-name">{name}</td>
       <td>{description}</td>
+      {reserved ? (<td className="align-verticle"><span className="active-member">Active member</span></td>) : (<td className="align-verticle"><span className="not-member">NOT A MEMBER</span></td>)}
+      {reserved ? (<td className="align-verticle"><button type="button" className="leave-mission">Leave Mission</button></td>) : (<td className="align-verticle"><button type="button" className="join-mission">Join Mission</button></td>)}
     </tr>
   );
 };
@@ -20,6 +23,7 @@ MissionTable.propTypes = {
   description: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default MissionTable;

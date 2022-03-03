f10d03e8f4a3679d893b533da7352cba7b0e56d5
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-03-03 17:54:55 +0500

    Display reserved mission in profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Profile() {
+  const missions = useSelector((state) => state.missionsReducer);
+  // const rockets = useSelector((state) => state.rocketsReducer);
+
+  // const rocketsReserved = rockets.filter((rocket) => rocket.reserved === true);
+
   return (
-    <div>Profile page in progress</div>
+    <div className="profile-container flex">
+      <div className="missions-container">
+        <h1>My Missions</h1>
+        <ul>
+          {missions.filter((mission) => mission.reserved === true).map((item) => (
+            item.reserved ? (<li>{item.name}</li>) : (<h2>No MIssions are added...</h2>)
+          ))}
+        </ul>
+      </div>
+      <div className="rockets-container">
+        <h1>My Rockets</h1>
+        <ul>
+          <li>hebehdbeh</li>
+        </ul>
+      </div>
+    </div>
   );
 }

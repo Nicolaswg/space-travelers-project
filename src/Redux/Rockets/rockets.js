31a2781df878fcec9313e9bb4bc2f51938c1524d
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2022-03-02 20:01:03 -0400

    Change rocket reserved status || created action for reserve and cancel reserve

--- src/Redux/Rockets/rockets.js
+++ src/Redux/Rockets/rockets.js
@@ -1,4 +1,6 @@
 const GET_ROCKETS_API = 'SpaceTravelersHub/missions/GET_ROCKETS_API';
+const ADD_ROCKETS_RESERVATION_API = 'SpaceTravelersHub/missions/ADD_ROCKETS_RESERVATION_API';
+const CANCEL_ROCKETS_RESERVATION_API = 'SpaceTravelersHub/missions/CANCEL_ROCKETS_RESERVATION_API';
 const ROCKET_API = 'https://api.spacexdata.com/v3/rockets';
 
 const initialState = [];
@@ -6,6 +8,16 @@ const initialState = [];
 export const getRockets = (payload) => ({
   type: GET_ROCKETS_API,
   payload,
+});
+
+export const addReservation = (id) => ({
+  type: ADD_ROCKETS_RESERVATION_API,
+  id,
+});
+
+export const cancelReservation = (id) => ({
+  type: CANCEL_ROCKETS_RESERVATION_API,
+  id,
 });
 
 export const getRocketsFromAPI = () => async (dispatch) => {
@@ -29,6 +41,20 @@ const rocketsReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS_API:
       return [...action.payload];
+    case ADD_ROCKETS_RESERVATION_API: {
+      const rocketState = state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: true };
+      });
+      return [...rocketState];
+    }
+    case CANCEL_ROCKETS_RESERVATION_API: {
+      const rocketState = state.map((rocket) => {
+        if (rocket.id !== action.id) return rocket;
+        return { ...rocket, reserved: false };
+      });
+      return [...rocketState];
+    }
     default:
       return state;
   }
